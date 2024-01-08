0eb1b4e4d6d7f3f8f9ac388c08cb87375b414ec0
TideS <tidesmain@gmail.com>
2024-01-09 03:16:45 +0500

    Whatever

--- cmake_generate_clang.bat
+++ cmake_generate_clang.bat
@@ -1,1 +1,1 @@
-cmake . -D CMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++" -D CMAKE_BUILD_TYPE=Release -G "Ninja" -B .\build\
+cmake . -D CMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++" -D CMAKE_BUILD_TYPE=Debug -G "Ninja" -B .\build\
